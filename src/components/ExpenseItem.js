c4c767b4d8c3dbcc73f707fdb4613f71ca0b43c7
Raey Faldo <raey3221@gmail.com>
2021-10-15 07:11:24 +0700

    use props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date();
-  const expenseTitle = 'Asuransi Mobil';
-  const expenseAmount = 1000000;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>Rp.{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>Rp.{props.amount}</div>
       </div>
     </div>
   );
