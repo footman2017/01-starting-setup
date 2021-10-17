2dad913135ef08c5e86dc6080e1395dffb00a35e
Raey Faldo <raey3221@gmail.com>
2021-10-17 10:33:24 +0700

    using map to render expense item

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,30 +3,16 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.data[0].title}
-        amount={props.data[0].amount}
-        date={props.data[0].date}
-      />
-      <ExpenseItem
-        title={props.data[1].title}
-        amount={props.data[1].amount}
-        date={props.data[1].date}
-      />
-      <ExpenseItem
-        title={props.data[2].title}
-        amount={props.data[2].amount}
-        date={props.data[2].date}
-      />
-      <ExpenseItem
-        title={props.data[3].title}
-        amount={props.data[3].amount}
-        date={props.data[3].date}
-      />
-    </Card>
-  );
+  const listExpenseItem = props.data.map((item) => (
+    <ExpenseItem
+      key={item.id}
+      title={item.title}
+      amount={item.amount}
+      date={item.date}
+    />
+  ));
+
+  return <Card className="expenses">{listExpenseItem}</Card>;
 };
 
 export default Expenses;
