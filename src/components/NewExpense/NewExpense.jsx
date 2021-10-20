deea4fa79a777d87d8d32b5b669ab33fdbb3aec1
Raey Faldo <raey3221@gmail.com>
2021-10-20 18:22:41 +0700

    fixing assignment 4

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,18 +1,43 @@
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [showForm, setshowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setshowForm(false);
   };
+
+  const showFormHandler = () => {
+    setshowForm(true);
+  };
+
+  const bttnCancelHandler = () => {
+    setshowForm(false);
+  };
+
+  if (showForm === true) {
+    return (
+      <div className="new-expense">
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={bttnCancelHandler}
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <button type="button" onClick={showFormHandler}>
+        Add New Expense
+      </button>
     </div>
   );
 };
